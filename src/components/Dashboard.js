b61d9c276770f4e21b28c1156c0181aba7e5ab9f
Jack Stubblefield <92601085+Jacks-01@users.noreply.github.com>
2022-08-17 14:23:14 -0500

    uncommented dashboard route and connected dashboard to server

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,5 +1,27 @@
-// import React from "react";
-// import Modal from './Modal.js';
-// import Header from './Header.js';
-// import Footer from './Footer.js';
-//need something here to track the habits 
+import React, { Component } from 'react';
+import axios from 'axios';
+const SERVER = process.env.REACT_APP_SERVER_URL;
+
+class Dashboard extends Component {
+
+    componentDidMount = () => {
+        this.getBackend();
+    }
+
+    getBackend = async () => {
+        await axios.get(`${SERVER}/`)
+        .then((res) => { 
+            console.log(res.data);
+            
+        }).catch((err) => {
+            console.error(err);
+        });
+    }
+    render() { 
+        return ( 
+            <h1>Dashboard</h1>
+         );
+    }
+}
+ 
+export default Dashboard;
