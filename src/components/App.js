f749a888e8f0e9dfce0d8ce50922d2bb8729435d
Teresa <luvmargaritas@gmail.com>
2022-08-17 15:41:02 -0500

    added home page. Corrected position of header in apps.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,14 @@ import Header from './Header.js';
 // import Footer from './Footer.js';
 // import Progress from './Progress.js';
 // import Dashboard from './Dashboard.js';
-// import Home from './Home.js';
+import Home from './Home.js';
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        {/* <Home/> */}
-        <Header/>
+        <Header />
+        {<Home />}
         {/* <Dashboard/>
         <Progress/>
         <About/>
