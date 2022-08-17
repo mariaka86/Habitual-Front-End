dcfe87abb8899433abbff66be3ea23a42e00f316
Jack Stubblefield <92601085+Jacks-01@users.noreply.github.com>
2022-08-17 15:40:36 -0500

    added chakra themeing and components to dashboard.js and index.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,27 +1,64 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Container } from '@chakra-ui/react';
+import { Alert, AlertIcon, Heading } from '@chakra-ui/react';
+import { Grid, GridItem } from '@chakra-ui/react';
+
 const SERVER = process.env.REACT_APP_SERVER_URL;
 
 class Dashboard extends Component {
+	constructor(props) {
+		super(props);
 
-    componentDidMount = () => {
-        this.getBackend();
-    }
+		this.state = {
+			user: undefined,
+			habits: 0
+		};
+	}
 
-    getBackend = async () => {
-        await axios.get(`${SERVER}/`)
-        .then((res) => { 
-            console.log(res.data);
-            
-        }).catch((err) => {
-            console.error(err);
-        });
-    }
-    render() { 
-        return ( 
-            <h1>Dashboard</h1>
-         );
-    }
+	componentDidMount = () => {
+		this.getBackend();
+	};
+
+	getBackend = async () => {
+		await axios
+			.get(`${SERVER}/`)
+			.then((res) => {
+				console.log(res.data);
+			})
+			.catch((err) => {
+				console.error(err);
+			});
+    };
+    
+    
+	render() {
+		const totalHabits = this.state.habits;
+		return (
+			<>
+				<Heading>This will be the dashboard for our habits!</Heading>
+
+                <Container>
+                    <Grid templateColumns='repeat(5, 1fr)' gap={6}>
+                        <GridItem w='100%' h='10' bg='red.400'/>
+                        <GridItem w='100%' h='10' bg='red.400'/>
+                        <GridItem w='100%' h='10' bg='red.400'/>
+                        <GridItem w='100%' h='10' bg='red.400'/>
+                        <GridItem w='100%' h='10' bg='red.400'/>
+                    </Grid>
+                </Container>
+
+
+
+				{totalHabits === 0 && (
+					<Alert status='error'>
+						<AlertIcon />
+						Looks like you don't have any habits!
+					</Alert>
+				)}
+			</>
+		);
+	}
 }
- 
+
 export default Dashboard;
--- src/index.js
+++ src/index.js
@@ -3,16 +3,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import './index.css';
 import App from './components/App';
+import { ChakraProvider } from '@chakra-ui/react';
+import theme from './theme';
 
 
 ReactDOM.render(
-
-  <Auth0Provider
-    domain={process.env.REACT_APP_AUTH_DOMAIN}
-    clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
-    redirectUri={process.env.REACT_APP_AUTH_REDIRECT_URI_REACT_SITE_URL}
-  >
-    <App />
-  </Auth0Provider>,
-  document.getElementById('root'),
+	<Auth0Provider
+		domain={process.env.REACT_APP_AUTH_DOMAIN}
+		clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
+		redirectUri={process.env.REACT_APP_AUTH_REDIRECT_URI_REACT_SITE_URL}
+	>
+		<ChakraProvider theme={theme}>
+			<App />
+		</ChakraProvider>
+	</Auth0Provider>,
+	document.getElementById('root')
 );
