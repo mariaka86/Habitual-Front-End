237cf7582959e7466ed57d480f0278fe718503ab
Teresa <luvmargaritas@gmail.com>
2022-08-18 16:41:50 -0500

    working on styling header

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,33 +9,36 @@ import Dashboard from "./Dashboard.js";
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+//import Button from 'react-bootstrap/Button';
 
 class Header extends React.Component {
   render() {
     return (
       <>
-    <Navbar>
-      <Container>
-      <Nav.Item> 
-      <AuthButton/>
-      </Nav.Item>
-      </Container>
-      <Container>
-      <h1>Habitual</h1>
-      </Container>
-      <Router>
-        { <Link to="/">Home</Link> }
-        <Link to="/Dashboard">Add a Habit</Link>
-        {/* <Link to="/Progress">Check Progress</Link> */}
-        <Link to="/About">About Us</Link>
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/Dashboard" element={<Dashboard />} />
-          {/* <Route exact path="/Progress" element={<Progress />} /> */}
-          <Route exact path="/About" element={<About />} />
-        </Routes>
-      </Router>
-      </Navbar>
+        <Navbar style={{margins: 'auto', min: '200vh'}}>
+          <Container>
+            <Nav.Item style={{align: 'right'}}>
+              <AuthButton />
+            </Nav.Item>
+          </Container>
+          <Container style={{fontSize: '30px'}}>
+            <Navbar.Brand href="/home">Habitual</Navbar.Brand>
+          </Container>
+          <Container>
+          <Router>
+            {<Link to="/">Home</Link>}
+            <Link to="/Dashboard">Add a Habit</Link>
+            {/* <Link to="/Progress">Check Progress</Link> */}
+            <Link to="/About">About Us</Link>
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route exact path="/Dashboard" element={<Dashboard />} />
+              {/* <Route exact path="/Progress" element={<Progress />} /> */}
+              <Route exact path="/About" element={<About />} />
+            </Routes>
+          </Router>
+          </Container>
+        </Navbar>
       </>
     );
   }
