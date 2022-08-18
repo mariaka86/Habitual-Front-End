aef383cc7f81a4dfaf3ce002e514bcc13c9c147a
Teresa <luvmargaritas@gmail.com>
2022-08-18 17:04:54 -0500

    add bootstrap button

--- src/components/LogoutButton.js
+++ src/components/LogoutButton.js
@@ -1,13 +1,15 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
+import {Button} from 'react-bootstrap';
+
 
 const LogoutButton = () => {
   const { logout } = useAuth0();
 
   return (
-    <button onClick={() => logout({ returnTo: window.location.origin })}>
+    <Button onClick={() => logout({ returnTo: window.location.origin })}>
       Log Out
-    </button>
+    </Button>
   );
 };
 
