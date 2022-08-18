c6c3f655d1fed629c6fb5cd50596c99141cb3dcc
MARY <marymwihaki123@gmail.com>
2022-08-18 14:25:10 -0500

    finished about page

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,8 @@ import image from '../images/marypic.JPG';
 import image1 from '../images/alliepic.jpg';
 import image2 from '../images/jackpic.png';
 import image3 from '../images/teresapic.jpg';
-import { SimpleGrid, Box } from '@chakra-ui/react'
+import { SimpleGrid, Box, Button} from '@chakra-ui/react'
+import {FaGithub} from 'react-icons/fa';
 
 class About extends React.Component {
 
@@ -20,7 +21,7 @@ class About extends React.Component {
                             <Card.Body>
                                 <Card.Img variant="top" src={image} />
                                 <Card.Title>Mary</Card.Title>
-                                <Card.Subtitle className="mb-2 text-muted">https://github.com/mariaka86</Card.Subtitle>
+                                <Button className="mb-2 text-muted" class = "fab fa-github" url ='https://github.com/mariaka86'leftIcon={<FaGithub/>}/>
                                 <Card.Text>
                                     Mary graduated from Kirkwood in 2020 and has an associates in Allied Health Sciences(medical assisting). She had been working in the healthcare field for the last two years through the pandemic. But has now decided to switch careers and is now a student at the Delta V Codeschool pursuing a career in Software Development.
                                 </Card.Text>
@@ -32,7 +33,7 @@ class About extends React.Component {
                             <Card.Body>
                                 <Card.Img variant="top" src={image1} />
                                 <Card.Title>Allie</Card.Title>
-                                <Card.Subtitle className="mb-2 text-muted">https://github.com/AllieDunkel</Card.Subtitle>
+                                <Button className="mb-2 text-muted" class = "fab fa-github" url ='https://github.com/AllieDunkel'leftIcon={<FaGithub/>}/>
                                 <Card.Text>
                                     Allie graduated from Mount Mercy University in 2022. She has her Bachelors of Science degree with her major in psychology and her minors in Criminal Justice and Child Studies. She is now a student at Delta V Code school trying to pursue a career in Software Development
                                 </Card.Text>
@@ -44,7 +45,7 @@ class About extends React.Component {
                             <Card.Body>
                                 <Card.Img variant="top" src={image2} />
                                 <Card.Title>Jack</Card.Title>
-                                <Card.Subtitle className="mb-2 text-muted">https://github.com/Jacks-01</Card.Subtitle>
+                                <Button className="mb-2 text-muted" class = "fab fa-github" url ='https://github.com/Jacks-01'leftIcon={<FaGithub/>}/>
                                 <Card.Text>
                                     Jack is a student at DeltaV Code School studying software development. He's making the career transition into software development because tech has always been a large part of his personal life, and he wants a career that prioritizes learning and honing his skills. His background before coming to deltaV was working with both youth and adults in mental health services.Moving forward, He would like to use his growing skills to develop quality software that can improve lives.
                                 </Card.Text>
@@ -56,7 +57,7 @@ class About extends React.Component {
                             <Card.Body>
                                 <Card.Img variant="top" src={image3} />
                                 <Card.Title>Teresa</Card.Title>
-                                <Card.Subtitle className="mb-2 text-muted">https://github.com/phillipstm</Card.Subtitle>
+                                <Button className="mb-2 text-muted" class= "fab fa-github"url='https://github.com/phillipstm' leftIcon={<FaGithub/>}/>
                                 <Card.Text>
                                     She recently returned to the area after living the past seven years in the San Francisco Bay area. She is retired from Procter & Gamble after 22 years of service. Her most recent role with P&G was that of Analyst I (Technical and Administrative), supporting a NorCal regional sales team. Her team broke all sales records for four consecutive years. She is currently pursuing a Software Development certification through Delta V. Her hobbies include walking, biking, and gardening.
                                 </Card.Text>
