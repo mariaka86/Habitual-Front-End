df44c1666c5344d7eff4d5943d143535eccb2ce0
Teresa <luvmargaritas@gmail.com>
2022-08-18 17:57:43 -0500

    header layout done need to make links work

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,42 +2,51 @@ import { withAuth0 } from "@auth0/auth0-react";
 import React from "react";
 import AuthButton from "./AuthButton.js";
 import About from "./About.js";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+// import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./Home.js";
 import Dashboard from "./Dashboard.js";
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 //import Button from 'react-bootstrap/Button';
-import Progress from "./Progress.js";
+// import Progress from "./Progress.js";
+import { Switch, Route, Link } from 'react-router-dom';
 
 
 class Header extends React.Component {
   render() {
     return (
       <>
-        <Navbar style={{margins: 'auto', min: '200vh'}}>
+        <Navbar>
           <Container>
-            <Nav.Item style={{align: 'right'}}>
-              <AuthButton />
-            </Nav.Item>
+            <Navbar.Brand href="#Habitual">Habitual</Navbar.Brand>
           </Container>
-          <Container style={{fontSize: '30px'}}>
-            <Navbar.Brand href="/home">Habitual</Navbar.Brand>
-          </Container>
+        </Navbar>
+        <br />
+        <Navbar>
           <Container>
-          <Router>
-            {<Link to="/">Home</Link>}
-            <Link to="/Dashboard">Add a Habit</Link>
-            <Link to="/Progress">Check Progress</Link>
-            <Link to="/About">About Us</Link>
-            <Routes>
-              <Route exact path="/" element={<Home />} />
-              <Route exact path="/Dashboard" element={<Dashboard />} />
-              <Route exact path="/Progress" element={<Progress />} />
-              <Route exact path="/About" element={<About />} />
-            </Routes>
-          </Router>
+
+            <Nav className="me-auto">
+
+              <Nav.Item eventkey={1} href="/">
+                <Nav.Link as={Link} to="/">Home</Nav.Link>
+              </Nav.Item>
+
+              <Nav.Item eventkey={2} href="/">
+                <Nav.Link as={Link} to="/Dashboard">Add a Habit</Nav.Link>
+              </Nav.Item>
+
+              
+              <Nav.Link href="#Progress">Check Progress</Nav.Link>
+              <Nav.Link href="#About">About Us</Nav.Link>
+            </Nav>
+            <Navbar.Brand href="#home"><AuthButton /></Navbar.Brand>
+          </Container>
+        </Navbar>
+        <br />
+        <Navbar bg="light" variant="light">
+          <Container>
+            <Home />
           </Container>
         </Navbar>
       </>
@@ -48,3 +57,11 @@ class Header extends React.Component {
 
 
 export default withAuth0(Header);
+
+
+/** 
+ * 
+
+
+
+*/
