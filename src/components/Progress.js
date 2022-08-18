a8d57f0f655e3603085adce8788e2fe90698bcec
Allie Dunkel <alliedunkel@outlook.com>
2022-08-18 14:06:07 -0500

    added the chart

--- src/components/Progress.js
+++ src/components/Progress.js
@@ -1,5 +1,36 @@
-// import React from "react";
+import React from "react";
 // import Header from './Header.js';
 // import Footer from './Footer.js';
 //import Dashboard from './Dashboard.js';
-//will need something here for the chart 
+import { Bar } from "react-chartjs-2";
+
+
+class HabitChart extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {};
+  // }
+  render() {
+    return (
+      <div>
+        <Bar
+          data={}
+          options={{
+            plugins: {
+              title: {
+                display: true,
+                text: "Your Habit Progress",
+              },
+              legend: {
+                display: true,
+                position: "bottom",
+              },
+            },
+          }}
+        />
+      </div>
+    );
+  }
+}
+
+export default HabitChart;
