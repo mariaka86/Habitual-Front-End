f92203d77f8b0542f9bcbff3f763f239af84334c
Teresa <luvmargaritas@gmail.com>
2022-08-19 09:52:27 -0500

    added routes to app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,32 @@
 import React from 'react';
 import Header from './Header.js';
-// import About from './About.js';
-// import Footer from './Footer.js';
-// import Progress from './Progress.js';
-// import Dashboard from './Dashboard.js';
-// import Home from './Home.js';
+import About from './About.js';
+//import Footer from './Footer.js';
+import Progress from './Progress.js';
+import Dashboard from './Dashboard.js';
+import Home from './Home.js';
+import { Routes, Route } from "react-router-dom";
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <Header />
+        <Routes>
+          <Route path="/Home" element={<Home />} />
+          <Route path="/Dashboard" element={<Dashboard />} />
+          <Route path="/Progress" element={<Progress />} />
+          <Route path="/About" element={<About />} />
+
+
+        </Routes>
         {/* {<Home />} */}
         {/* <Dashboard/>
         <Progress/>
         <About/>
         <Footer/>*/}
+
+
       </div>
     );
   }
