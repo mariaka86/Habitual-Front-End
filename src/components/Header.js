9aafadaefd565745f05de6d11dbae958a2dbac02
Teresa <luvmargaritas@gmail.com>
2022-08-19 09:55:04 -0500

    update imports on Hdr

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,8 @@
 import { withAuth0 } from "@auth0/auth0-react";
 import React from "react";
 import AuthButton from "./AuthButton.js";
-import About from "./About.js";
-// import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Home from "./Home.js";
-import Dashboard from "./Dashboard.js";
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
-//import Button from 'react-bootstrap/Button';
-// import Progress from "./Progress.js";
-import { Switch, Route, Link } from 'react-router-dom';
+import {Container, Navbar, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 
 class Header extends React.Component {
