44af9ebd18bff944c7eb017c7d88f3fb84401c56
Teresa <luvmargaritas@gmail.com>
2022-08-19 10:27:11 -0500

    app is landing page but not home

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Header from './Header.js';
 import About from './About.js';
-//import Footer from './Footer.js';
+import Footer from './Footer.js';
 import Progress from './Progress.js';
 import Dashboard from './Dashboard.js';
 import Home from './Home.js';
 import { Routes, Route } from "react-router-dom";
+
 
 class App extends React.Component {
   render() {
@@ -17,16 +18,8 @@ class App extends React.Component {
           <Route path="/Dashboard" element={<Dashboard />} />
           <Route path="/Progress" element={<Progress />} />
           <Route path="/About" element={<About />} />
-
-
         </Routes>
-        {/* {<Home />} */}
-        {/* <Dashboard/>
-        <Progress/>
-        <About/>
-        <Footer/>*/}
-
-
+        <Footer/>
       </div>
     );
   }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,9 @@
 import { withAuth0 } from "@auth0/auth0-react";
 import React from "react";
 import AuthButton from "./AuthButton.js";
-import {Container, Navbar, Nav } from 'react-bootstrap';
+import { Container, Navbar, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+
 
 
 class Header extends React.Component {
@@ -17,30 +18,16 @@ class Header extends React.Component {
         <br />
         <Navbar>
           <Container>
-
             <Nav className="me-auto">
-
-              <Nav.Item eventkey={1} href="/">
-                <Nav.Link as={Link} to="/">Home</Nav.Link>
-              </Nav.Item>
-
-              <Nav.Item eventkey={2} href="/">
-                <Nav.Link as={Link} to="/Dashboard">Add a Habit</Nav.Link>
-              </Nav.Item>
-
-              
-              <Nav.Link href="#Progress">Check Progress</Nav.Link>
-              <Nav.Link href="#About">About Us</Nav.Link>
+              <Nav.Link as={Link} to="/home">Home</Nav.Link>
+              <Nav.Link as={Link} to="/dashboard">Add a Habit</Nav.Link>
+              <Nav.Link as={Link} to="/progress">Check Progress</Nav.Link>
+              <Nav.Link as={Link} to="/about">About Us</Nav.Link>
             </Nav>
             <Navbar.Brand href="#home"><AuthButton /></Navbar.Brand>
           </Container>
         </Navbar>
-        <br />
-        <Navbar bg="light" variant="light">
-          <Container>
-            <Home />
-          </Container>
-        </Navbar>
+       
       </>
     );
   }
